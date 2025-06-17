9fe82fcd54c63980a080f9b79b8b8ff916b2316c
Olusola Moyosore <olusolamoyosore@Olusolas-MacBook-Pro.local>
2025-06-17 20:03:33 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "@/components/sections/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,6 +52,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${franklinGothic.variable} font-franklin antialiased`}
       >
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,6 @@ export default function Home() {
       <Tattoo />
       <Lifestyle />
       <HoodSkool />
-      <Footer />
-      {/* other sections */}
     </main>
   );
 }
